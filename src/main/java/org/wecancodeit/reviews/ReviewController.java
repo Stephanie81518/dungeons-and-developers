22ce658398549d2451371c7700b1dacad4977a5c
Michael Taylor <mikejtaylor22@gmail.com>
2021-01-11 15:17:59 -0500

    Removing dependencies

--- src/main/java/org/wecancodeit/reviews/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewController.java
@@ -1,163 +1,68 @@
 package org.wecancodeit.reviews;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class ReviewController {
-Map<Integer,Game> tableTopGames = new HashMap<>();
-Map<Integer,Game> videoGames = new HashMap<>();
-Map<Integer,Game> cardGames = new HashMap<>();
-//test
-Collection<Game> actionGames = new ArrayList<>();
-Collection<Game> rpg = new ArrayList<>();
-Collection<Game> exploration = new ArrayList<>();
-Collection<Game> party = new ArrayList<>();
 
-public String category;
+    public ReviewController() {
 
-public ReviewController(){
-    Game dnd = new Game("Dungeons and Dragons","An open world RPG game",this.category,"RPG",1);
-    Game betrayal= new Game("Betrayal on the house on Haunted Hill","A Fun Party Game",this.category,"Party",2);
-    Game monopoly = new Game("Monopoly", "Players collect rent from their opponents, with the goal being to drive them into bankruptcy",
-            this.category,"Horror",3);
-    tableTopGames.put(dnd.getId(),dnd);
-    tableTopGames.put(betrayal.getId(),betrayal);
-    tableTopGames.put(monopoly.getId(),monopoly);
-
-    Game amongUs = new Game("Among Us", "Find the imposter before they destroy your ship!",this.category,"Party",4);
-    Game hades = new Game("Hades", "Destroy the Demons before they Destroy you!",this.category,"Exploration",5);
-    Game detroit = new Game("Detroit Become Human", "A Choose your own adventure game.",this.category,"RPG",6);
-    videoGames.put(amongUs.getId(),amongUs);
-    videoGames.put(hades.getId(),hades);
-    videoGames.put(detroit.getId(),detroit);
-
-    Game cardsAgainst = new Game("Cards Against Humanity", "A Fun party game!",this.category,"Party",7);
-    Game superFights = new Game("SuperFights", "Fight your friends with cards!",this.category,"Action",8);
-    Game uno = new Game("Uno", "Ruin your friendships with UNO!",this.category,"Action",9);
-
-    cardGames.put(cardsAgainst.getId(),cardsAgainst);
-    cardGames.put(superFights.getId(),superFights);
-    cardGames.put(uno.getId(),uno);
-
-}
-
-@RequestMapping("Action")
-public String actionTag(Model inModel){
-    actionGames.clear();
-    for(Game g: tableTopGames.values()){
-        if(g.getHashtag().equals("Action")){
-            actionGames.add(g);
-        }
     }
-    for(Game g: videoGames.values()){
-        if(g.getHashtag().equals("Action")){
-            actionGames.add(g);
-        }
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @RequestMapping("Action")
+    public String actionTag(Model inModel) {
+
+        inModel.addAttribute("actionTags", reviewService.getActionGames());
+        return "Action-template";
     }
-    for(Game g: cardGames.values()){
-        if(g.getHashtag().equals("Action")){
-            actionGames.add(g);
-        }
-    }
-    inModel.addAttribute("actionTags",actionGames);
-return "Action-template";
-}
 
     @RequestMapping("RPG")
-    public String rpgHashtag(Model inModel){
-    rpg.clear();
-        for(Game g: tableTopGames.values()){
-            if(g.getHashtag().equals("RPG")){
-                rpg.add(g);
-            }
-        }
-        for(Game g: videoGames.values()){
-            if(g.getHashtag().equals("RPG")){
-                rpg.add(g);
-            }
-        }
-        for(Game g: cardGames.values()){
-            if(g.getHashtag().equals("RPG")){
-                rpg.add(g);
-            }
-        }
-        inModel.addAttribute("rpgTags",rpg);
+    public String rpgHashtag(Model inModel) {
+        inModel.addAttribute("rpgTags", reviewService.getRpg());
         return "Rpg-template";
     }
 
     @RequestMapping("Party")
-    public String partyHashtag(Model inModel){
-        party.clear();
-        for(Game g: tableTopGames.values()){
-            if(g.getHashtag().equals("Party")){
-                party.add(g);
-            }
-        }
-        for(Game g: videoGames.values()){
-            if(g.getHashtag().equals("Party")){
-                party.add(g);
-            }
-        }
-        for(Game g: cardGames.values()){
-            if(g.getHashtag().equals("Party")){
-                party.add(g);
-            }
-        }
-        inModel.addAttribute("partyTags",party);
+    public String partyHashtag(Model inModel) {
+        inModel.addAttribute("partyTags", reviewService.getParty());
         return "Party-template";
     }
 
     @RequestMapping("Exploration")
-    public String explorationHashtag(Model inModel){
-        exploration.clear();
-        for(Game g: tableTopGames.values()){
-            if(g.getHashtag().equals("Exploration")){
-                exploration.add(g);
-            }
-        }
-        for(Game g: videoGames.values()){
-            if(g.getHashtag().equals("Exploration")){
-                exploration.add(g);
-            }
-        }
-        for(Game g: cardGames.values()){
-            if(g.getHashtag().equals("Exploration")){
-                exploration.add(g);
-            }
-        }
-        inModel.addAttribute("explorationTags",exploration);
+    public String explorationHashtag(Model inModel) {
+        inModel.addAttribute("explorationTags", reviewService.getExploration());
         return "Exploration-template";
     }
 
     @RequestMapping("TableTopCategory")
     public String getTableTop(Model inModel) {
-        this.category = "TableTop";
-        inModel.addAttribute("category",this.category);
-        inModel.addAttribute("games",tableTopGames.values());
+
+        inModel.addAttribute("category", "Table Top");
+        inModel.addAttribute("games", reviewService.getTableTopGames());
         return "Review-template";
 
     }
 
     @RequestMapping("VideoGameCategory")
-    public String getVideoGame(Model inModel){
-        this.category = "VideoGame";
-        inModel.addAttribute("Videogames",videoGames.values());
-        inModel.addAttribute("category",this.category);
+    public String getVideoGame(Model inModel) {
+        inModel.addAttribute("Videogames", reviewService.getVideoGames());
+        inModel.addAttribute("category", "Video Games");
         return "VideoGame-template";
 
     }
+
     @RequestMapping("CardGameCategory")
-    public String getCardGame(Model inModel){
-        this.category = "CardGame";
-        inModel.addAttribute("Cardgames",cardGames.values());
-        inModel.addAttribute("category",this.category);
+    public String getCardGame(Model inModel) {
+
+        inModel.addAttribute("Cardgames", reviewService.getCardGames());
+        inModel.addAttribute("category", "Card Games");
         return "CardGameCategory-template";
     }
 
